f6aec01cb1c4b5ac6deb4fa3d8f41d0079119878
winlin <winlin@vip.126.com>
2020-10-10 13:46:04 +0800

    Update

--- auto/release.sh
+++ auto/release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git tag -d release-vubuntu16
-git tag release-vubuntu16
-git push origin -f release-vubuntu16
-git push aliyun -f release-vubuntu16
+git tag -d release-vubuntu20
+git tag release-vubuntu20
+git push origin -f release-vubuntu20
+git push aliyun -f release-vubuntu20
